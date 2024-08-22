374afae4779e3b98d9d06af6068c67abcde3e515
Guy Micciche <king.guy@outlook.com>
2024-08-21 23:34:08 -0600

    Update functions.py

--- wol/functions.py
+++ wol/functions.py
@@ -38,40 +38,32 @@ def daily_text():
 
 def process_daily_text(html_content):
     """Processes the daily text from the provided HTML content."""
-    # Get the current date
-    today = datetime.today()
-    
-    # Format the date to match the "data-date" attribute in the HTML
-    formatted_date = today.strftime('%Y-%m-%dT00:00:00.000Z')
-    
+   
     # Parse the HTML with BeautifulSoup
     soup = BeautifulSoup(html_content)
     
-    # Determine the index for "today"
-    day = 1  # Index 1 for today in the list of daily texts
-    
-    # Find the h2 tags and get the appropriate one based on 'day'
-    h2_tags = soup.find_all('h2')
-    h2_text = h2_tags[day].get_text(strip=True) if len(h2_tags) > day else 'Not Found'
-    
-    # Find the p tags with the class "themeScrp"
-    themeScrp_tags = soup.find_all('p', class_='themeScrp')
-    themeScrp_text = themeScrp_tags[day].get_text(strip=True) if len(themeScrp_tags) > day else 'Not Found'
-    
-    # Find the div tag with the appropriate data-date based on the formatted_date
-    tabContent_div = soup.find('div', attrs={"data-date": formatted_date})
-    tabContent_text = tabContent_div.get_text(strip=True) if tabContent_div else 'Not Found'
-    
-    # Extracting scripture from themeScrp_text based on pattern
+    # Extract the date text from the first <h2> tag
+    date_tag = soup.find('h2')  # This gets the first <h2> tag
+    date_text = h2_tag.get_text(strip=True) if h2_tag else 'Not Found'
+
+    # Extract the theme scripture text from the <p> tag with class "themeScrp"
+    themeScrp_tag = soup.find('p', class_='themeScrp')
+    themeScrp_text = themeScrp_tag.get_text(strip=True) if themeScrp_tag else 'Not Found'
+
+    # Extract the main body text from the <div> tag with class "bodyTxt"
+    bodyTxt_div = soup.find('div', class_='bodyTxt')
+    bodyTxt_text = bodyTxt_div.get_text(strip=True) if bodyTxt_div else 'Not Found'
+
+    # Extracting scripture reference from themeScrp_text based on pattern
     scripture_match = re.search(r'—(.*? \d+:\d+)', themeScrp_text)
     scripture_text = scripture_match.group(1) if scripture_match else 'Not Found'
     
     # Prepare the result
     result = {
-        'dateText': h2_text,
+        'dateText': date_text,
         'scriptureFull': themeScrp_text,
         'scriptureReference': scripture_text,
-        'dailyText': tabContent_text
+        'dailyText': bodyTxt_text
     }
     
     # Return the result as JSON

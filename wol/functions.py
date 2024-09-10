dbf518e129ebac07424c0e5c98208d5192f35d66
Guy Micciche <king.guy@outlook.com>
2024-09-09 19:32:05 -0600

    Update functions.py

--- wol/functions.py
+++ wol/functions.py
@@ -1,13 +1,71 @@
 import re
 import requests
+import gzip
+import json
+from io import BytesIO
 from datetime import datetime
 from flask import Blueprint, jsonify, request
 from bs4 import BeautifulSoup
 
 functions_bp = Blueprint('functions', __name__)
 
-@functions_bp.route('/daily-text', methods=['GET', 'POST'])
-def daily_text():
+@functions_bp.route("/catalog", methods=['GET'], defaults={'language': 'E'})
+@functions_bp.route("/catalog/<language>", methods=['GET'])
+def catalog(language='E'):
+    url = f"https://app.jw-cdn.org/catalogs/media/{language}.json.gz"
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    with gzip.GzipFile(fileobj=BytesIO(response.content)) as gz:
+        data = gz.read().decode('utf-8')
+
+    items = [json.loads(item)['o']
+             for item in data.splitlines()
+             if json.loads(item)["type"] == "media-item"
+             and json.loads(item).get('o', {}).get('keyParts', {}).get('formatCode') == 'VIDEO']
+
+    return jsonify(items)
+
+# Example GET: http://localhost:5000/mediaitems?languages=E,CHS&mediaItems=pub-iam-1_10_AUDIO,pub-jwb-080_9_VIDEO,pub-jwb-081_10_VIDEO,pub-jwb-081_11_VIDEO,pub-jwb-081_1_AUDIO,pub-jwb-081_1_VIDEO
+# Example POST: {"languages": ["E", "CHS"], "mediaItems": ["pub-iam-1_10_AUDIO", "pub-jwb-080_9_VIDEO"}]}
+@functions_bp.route("/mediaitems", methods=['POST', 'GET'])
+def mediaitems():
+    try:
+        if request.method == 'POST':
+            data = request.get_json()
+            
+        else:
+            data = request.args
+
+        lang_str = data.get('languages')
+        media_str = data.get('mediaItems')
+        languages = lang_str.split(',') if lang_str else ['E']
+        mediaItems = media_str.split(',') if media_str else []
+
+        final_response = process_media_items(languages, mediaItems)
+
+        return jsonify(final_response)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Example GET: http://localhost:5000/mediaitem/E/pub-jwb-080_9_VIDEO
+@app.route("/mediaitem/<language>/<mediaItem>", methods=['GET'])
+def mediaitem(language, mediaItem):
+    try:
+        # If the route is /mediainfo/<language>/<mediaItem>
+        languages = [str(language)]
+        mediaItems = [str(mediaItem)]
+
+        final_response = process_media_items(languages, mediaItems)
+
+        return jsonify(final_response)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
+@functions_bp.route('/dailytext', methods=['GET', 'POST'])
+def dailytext():
     """Handles both GET and POST requests to retrieve and process daily text."""
     
     if request.method == 'POST':
@@ -36,6 +94,75 @@ def daily_text():
     # Process the HTML content
     return process_daily_text(html_input)
 
+def process_media_items(languages, mediaItems):
+    responses = []
+
+    for language in languages:
+        for mediaItem in mediaItems:
+            url = f"https://b.jw-cdn.org/apis/mediator/v1/media-items/{language}/{mediaItem}?clientType=www"
+            response = requests.get(url)
+            response.raise_for_status()
+
+                # Get the first image from the first image_type that has an image_size
+            for image_type in ["lsr", "cvr", "sqr"]:
+                images = response.json().get('media')[0].get('images').get(image_type)
+                if images:
+                    for image_size in ["xl", "lg", "md"]:
+                        image_url = images.get(image_size)
+                        if image_url:
+                            image_url = image_url
+                            break  # Stop after finding the first image
+                    if image_url: 
+                        break # Stop after finding an image in any image_type
+                    else:
+                        image_url = None
+                        break
+
+            largest_file = max([file for file in response.json().get('media')[0].get('files')], key=lambda f: f['filesize'])
+
+                # Create the custom dictionary
+            media_data = {
+                    "title": response.json().get('media')[0].get('title'),
+                    "primaryCategory": response.json().get('media')[0].get('primaryCategory'),
+                    "mediaCitation": response.json().get('media')[0].get('printReferences')[0],
+                    "languageAgnosticNaturalKey": response.json().get('media')[0].get('languageAgnosticNaturalKey'),
+                    "firstPublished": response.json().get('media')[0].get('firstPublished'),
+                    "type": response.json().get('media')[0].get('type'),
+                    "duration": response.json().get('media')[0].get('duration'),
+                    # "files": [
+                    #     {
+                    #         "filesize": file.get('filesize'),
+                    #         "progressiveDownloadURL": file.get('progressiveDownloadURL'),
+                    #         "subtitles": file.get('subtitles', {}).get('url')
+                    #     } for file in response.json().get('media')[0].get('files')
+                    # ], # Gets all files
+                    "file": largest_file["progressiveDownloadURL"],
+                    #"images": response.json().get('media')[0].get('images') # Gets all images
+                }
+
+                # Add subtitle field only if subtitles exist
+            if largest_file.get('subtitles'):
+                media_data["subtitle"] = largest_file.get('subtitles', {}).get('url')
+
+                # Add image field only if image exist
+            if image_url:
+                media_data["image"] = image_url
+
+            responses.append(media_data)
+                
+        # Create a list of dictionaries to represent the overall response
+    final_response = []
+    for lang in languages:
+        lang_response = {
+                "languageCode": lang,
+                "media": [
+                    media_data for media_data in responses
+                ]
+            }
+        final_response.append(lang_response)
+
+    return final_response
+    
 def process_daily_text(html_content):
     """Processes the daily text from the provided HTML content."""
    
@@ -61,8 +188,8 @@ def process_daily_text(html_content):
     # Prepare the result
     result = {
         'dateText': date_text,
-        'scriptureFull': themeScrp_text,
         'scriptureReference': scripture_text,
+        'scriptureFull': themeScrp_text,
         'dailyText': bodyTxt_text
     }
     
